f81b766d30f408cdae5888f718ec46e9b175b573
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-11-15 23:04:00 +0900

    indexの追加

---
+++ tpl/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>home | ボーリング</title>
+</head>
+<body>
+
+  <main>
+    <p><a href="./create_user.php">create user</a></p>
+    <p><a href="./game_play.php">game play</a></p>
+  </main>
+
+</body>
+</html>
