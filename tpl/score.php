7750b52a766185d26b5e2736abeaceee872e87f2
Hidaka Rintaro <rintaro0411bus@gmail.com>
2020-11-16 00:59:47 +0900

    scoreの追加

---
+++ tpl/score.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>score | ボーリング</title>
+</head>
+<body>
+  <table border="1">
+    <tr>
+      <td>name</td>
+      <td class="01" colspan="2"></td>
+      <td class="02" colspan="2"></td>
+      <td class="03" colspan="2"></td>
+      <td class="04" colspan="2"></td>
+      <td class="05" colspan="2"></td>
+      <td class="06" colspan="2"></td>
+      <td class="07" colspan="2"></td>
+      <td class="08" colspan="2"></td>
+      <td class="09" colspan="2"></td>
+      <td class="10" colspan="3"></td>
+    </tr>
+    <tr>
+      <td class="name" rowspan="2"></td>
+      <td class="f01_1"></td>
+      <td class="f01_2"></td>
+      <td class="f02_1"></td>
+      <td class="f02_2"></td>
+      <td class="f03_1"></td>
+      <td class="f03_2"></td>
+      <td class="f04_1"></td>
+      <td class="f04_2"></td>
+      <td class="f05_1"></td>
+      <td class="f05_2"></td>
+      <td class="f06_1"></td>
+      <td class="f06_2"></td>
+      <td class="f07_1"></td>
+      <td class="f07_2"></td>
+      <td class="f08_1"></td>
+      <td class="f08_2"></td>
+      <td class="f09_1"></td>
+      <td class="f09_2"></td>
+      <td class="f10_1"></td>
+      <td class="f10_2"></td>
+      <td class="f10_3"></td>
+    </tr>
+    <tr>
+      <td class="f01_s" colspan="2"></td>
+      <td class="f02_s" colspan="2"></td>
+      <td class="f03_s" colspan="2"></td>
+      <td class="f04_s" colspan="2"></td>
+      <td class="f05_s" colspan="2"></td>
+      <td class="f06_s" colspan="2"></td>
+      <td class="f07_s" colspan="2"></td>
+      <td class="f08_s" colspan="2"></td>
+      <td class="f09_s" colspan="2"></td>
+      <td class="f10_s" colspan="3"></td>
+    </tr>
+  </table>
+</body>
+</html>
